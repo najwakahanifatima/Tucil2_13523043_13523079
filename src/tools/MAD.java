aa76ce92151cfec2dd9ffe29e585ed19bad8fdab
Najwa Kahani Fatima <najwanewa1010@gmail.com>
2025-04-03 11:52:40 +0700

    feat: add class Mean Absolute Deviation algorithm

--- src/tools/MAD.java
+++ src/tools/MAD.java
@@ -1,3 +1,30 @@
 public class MAD {
-    
+    public static double computeMAD(int[][] red, int[][] green, int[][] blue){
+        double redMAD = computeMADCanal(red);
+        double greenMAD = computeMADCanal(green);
+        double blueMAD = computeMADCanal(blue);
+        
+        return (redMAD + greenMAD + blueMAD)/3;
+    }
+
+    public static double computeMADCanal(int[][] matrix){
+        int totElement = matrix.length * matrix[0].length;
+        int[] flatArray = new int[totElement];
+        int i = 0, sum = 0;
+
+        for (int[] row : matrix){
+            for (int value : row){
+                flatArray[i++] = value;
+                sum += value;
+            }
+        }
+        double mean = sum / totElement;
+
+        int sumDev = 0;
+        for (int value : flatArray){
+            sumDev += Math.abs(value - mean);
+        }
+
+        return (double) sumDev / totElement;
+    }
 }

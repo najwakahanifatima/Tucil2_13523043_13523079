7b9bc348d4a81d65f02f2ef4683415d825a8c650
Najwa Kahani Fatima <najwanewa1010@gmail.com>
2025-04-05 21:34:31 +0700

    fix: bug in MAD computation

--- src/errorMeasurement/MAD.java
+++ src/errorMeasurement/MAD.java
@@ -4,8 +4,7 @@ public class MAD {
     public static double computeMAD(int[][] red, int[][] green, int[][] blue, int rowTL, int colTL, int h, int w){
         double redMAD = computeMADCanal(red, rowTL, colTL, h, w);
         double greenMAD = computeMADCanal(green, rowTL, colTL, h, w);
-        double blueMAD = computeMADCanal(blue, rowTL, colTL, h, w);
-        
+        double blueMAD = computeMADCanal(blue, rowTL, colTL, h, w);        
         return (redMAD + greenMAD + blueMAD)/3;
     }
 
@@ -13,16 +12,10 @@ public class MAD {
         // adjust range index
         int totElement = w * h;
         int[] flatArray = new int[totElement];
-        // System.out.println("tot el " + totElement);
-        // System.out.println("h " + h);
-        // System.out.println("w " + w);
         int k = 0, sum = 0;
 
         for (int i = 0; i < h; i++) {
             for (int j = 0; j < w; j++) {
-                // System.out.println("k " + k);
-                // System.out.println("i + rowTL " + (i + rowTL));
-                // System.out.println("j + colTL " + (j + colTL));
                 flatArray[k++] = matrix[i + rowTL][j + colTL];
                 sum += matrix[i + rowTL][j + colTL];
             }

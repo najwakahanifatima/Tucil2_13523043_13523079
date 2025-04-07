e66d785eb7d9e96d8ce00c62176dc915c8f35a20
Najwa Kahani Fatima <najwanewa1010@gmail.com>
2025-04-08 04:07:21 +0700

    fix: update compiled file

--- run.bat
+++ run.bat
@@ -4,7 +4,7 @@
 if not exist bin mkdir bin
 
 :: Compile all Java files from src directory into bin
-javac -d bin src\errorMeasurement\*.java src\tools\*.java src\imageHandler\*.java src\*.java
+javac -d bin src\errorMeasurement\*.java src\tools\*.java src\*.java
 
 :: Run the main class
 java -cp bin Main

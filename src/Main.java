098c3f284608cbb948b6f07802d5bc29b1c83b4d
Najwa Kahani Fatima <najwanewa1010@gmail.com>
2025-04-11 14:01:48 +0700

    rename file main

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ import tools.Input;
 import tools.Quadtree;
 import tools.TargettedCompressor;
 
-public class Try {
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Input.displayWelcome();
